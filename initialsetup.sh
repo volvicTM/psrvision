ca026f8ece96d24fb055b657fcc217b90c609989
volvicTM <george.horlor@gmail.com>
2017-08-19 16:01:54 +0100

    Update initialsetup.sh

--- initialsetup.sh
+++ initialsetup.sh
@@ -14,7 +14,7 @@ read uname
 
 randompw=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
 
-adduser $uname --disabled-password --gecos
+useradd -d /home/"$uname" -m -g users -s /bin/bash "$uname"
 echo $uname:$randompw | chpasswd
 
 # Enter SSH Key
