c08e2cfac087ad7636611090a705dd97a5f89fea
volvicTM <george.horlor@gmail.com>
2017-08-20 09:41:36 +0100

    Update appinstall.sh

--- appinstall.sh
+++ appinstall.sh
@@ -170,6 +170,15 @@ docker run \
 -e VIRTUAL_HOST=plex.$durl \
 -e LETSENCRYPT_HOST=plex.$durl \
 -e LETSENCRYPT_EMAIL=$leemail \
+-p 32400:32400/tcp \
+-p 3005:3005/tcp \
+-p 8324:8324/tcp \
+-p 32469:32469/tcp \
+-p 1900:1900/udp \
+-p 32410:32410/udp \
+-p 32412:32412/udp \
+-p 32413:32413/udp \
+-p 32414:32414/udp \
 -e PLEX_UID="1000" \
 -e PLEX_GID="1000" \
 -v /home/$USER/Plex:/config \
