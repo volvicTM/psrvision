864b0c72f6db1aecda025fd4bf554005b1107357
volvicTM <george.horlor@gmail.com>
2017-08-23 07:10:59 +0100

    Update runfirst.sh

--- runfirst.sh
+++ runfirst.sh
@@ -1,31 +1,21 @@
 #!/bin/bash
 
 # Get Username and write to install scripts
-if read -p "Please enter a username: " uname; then
-  sed -i~ -e "s/USERNAME/${uname}/g" initialsetup.sh
-  sed -i~ -e "s/USERNAME/${uname}/g" appsetup.sh
-else
-  # Error
-fi
+read -p "Please enter a username: " uname
+sed -i~ -e "s/USERNAME/${uname}/g" initialsetup.sh
+sed -i~ -e "s/USERNAME/${uname}/g" appinstall.sh
+
 
 # Get Domain and write to install scripts
-if read -p "Please enter a your Domain name (e.g. mydomain.tv): " uurl; then
-  sed -i~ -e "s/USERURL/${uurl}/g" appsetup.sh
-else
-  # Error
-fi
+read -p "Please enter a your Domain name (e.g. mydomain.tv): " uurl
+sed -i~ -e "s/USERURL/${uurl}/g" appinstall.sh
 
 # Get Email and write to install scripts
-if read -p "Please enter a email address for let's encrypt: " uemail; then
-  sed -i~ -e "s/USEREMAIL/${uemail}/g" appsetup.sh
-else
-  # Error
-fi
+read -p "Please enter a email address for let's encrypt: " uemail
+sed -i~ -e "s/USEREMAIL/${uemail}/g" appinstall.sh
 
 # Get Basic Auth Username and write to install scripts
-if read -p "Please enter a username to access the websites, (like sonarr and radarr): " unginx; then
-  sed -i~ -e "s/USERBASICAUTH/${unginx}/g" appsetup.sh
-else
-  # Error
-fi
+read -p "Please enter a username to access the websites, (like sonarr and radarr): " unginx
+sed -i~ -e "s/USERBASICAUTH/${unginx}/g" appinstall.sh
+
 exit
