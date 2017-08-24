f9f80c4058b3864dc8e4062182efb29d5b11c6d7
volvicTM <george.horlor@gmail.com>
2017-08-24 17:02:01 +0100

    Update initialsetup.sh

--- initialsetup.sh
+++ initialsetup.sh
@@ -44,45 +44,6 @@ usermod -aG docker USERNAME
 # Secure fstab
 echo 'tmpfs /run/shm tmpfs defaults,noexec,nosuid 0 0' >> /etc/fstab
 
-# Source routing hardening
-cat >/etc/sysctl.conf <<'EOT'
-# IP Spoofing protection
-net.ipv4.conf.all.rp_filter = 1
-net.ipv4.conf.default.rp_filter = 1
-
-# Ignore ICMP broadcast requests
-net.ipv4.icmp_echo_ignore_broadcasts = 1
-
-# Disable source packet routing
-net.ipv4.conf.all.accept_source_route = 0
-net.ipv6.conf.all.accept_source_route = 0 
-net.ipv4.conf.default.accept_source_route = 0
-net.ipv6.conf.default.accept_source_route = 0
-
-# Ignore send redirects
-net.ipv4.conf.all.send_redirects = 0
-net.ipv4.conf.default.send_redirects = 0
-
-# Block SYN attacks
-net.ipv4.tcp_syncookies = 1
-net.ipv4.tcp_max_syn_backlog = 2048
-net.ipv4.tcp_synack_retries = 2
-net.ipv4.tcp_syn_retries = 5
-
-# Log Martians
-net.ipv4.conf.all.log_martians = 1
-net.ipv4.icmp_ignore_bogus_error_responses = 1
-
-# Ignore ICMP redirects
-net.ipv4.conf.all.accept_redirects = 0
-net.ipv6.conf.all.accept_redirects = 0
-net.ipv4.conf.default.accept_redirects = 0 
-net.ipv6.conf.default.accept_redirects = 0
-
-# Ignore Directed pings
-net.ipv4.icmp_echo_ignore_all = 1
-EOT
-
 # Prevent IP Spoofing
 sed -i 's/order hosts,bind/order bind,hosts/' /etc/host.conf
 sed -i 's/multi on/nospoof on/' /etc/host.conf
