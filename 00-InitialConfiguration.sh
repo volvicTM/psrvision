9b2c3d0af54c39b12f58450227e7f4c1236a3297
volvicTM <george.horlor@gmail.com>
2017-09-01 07:23:49 +0100

    Update 00-InitialConfiguration.sh

--- 00-InitialConfiguration.sh
+++ 00-InitialConfiguration.sh
@@ -3,20 +3,20 @@
 # Get Username and write to install scripts
 read -p "Please enter a username: " uname
 sed -i~ -e "s/USERNAME/${uname}/g" 01-Serverbase.sh
-sed -i~ -e "s/USERNAME/${uname}/g" appinstall.sh
+sed -i~ -e "s/USERNAME/${uname}/g" 02-Appsetup.sh
 
 
 # Get Domain and write to install scripts
 read -p "Please enter a your Domain name (e.g. mydomain.tv): " uurl
-sed -i~ -e "s/USERURL/${uurl}/g" appinstall.sh
+sed -i~ -e "s/USERURL/${uurl}/g" 02-Appsetup.sh
 
 # Get Email and write to install scripts
 read -p "Please enter a email address for let's encrypt: " uemail
-sed -i~ -e "s/USEREMAIL/${uemail}/g" appinstall.sh
+sed -i~ -e "s/USEREMAIL/${uemail}/g" 02-Appsetup.sh
 
 # Get Basic Auth Username and write to install scripts
 read -p "Please enter a username to access the websites, (like sonarr and radarr): " unginx
-sed -i~ -e "s/USERBASICAUTH/${unginx}/g" appinstall.sh
+sed -i~ -e "s/USERBASICAUTH/${unginx}/g" 02-Appsetup.sh
 
 chmod +x /home/plex/psrvision/01-Serverbase.sh
 chmod +x /home/plex/psrvision/02-Appsetup.sh
