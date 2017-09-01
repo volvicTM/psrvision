90a2454b85211d364b02587005ff144ed2ea9f95
volvicTM <george.horlor@gmail.com>
2017-09-01 07:11:14 +0100

    Update and rename runfirst.sh to 00-InitialConfiguration.sh

--- 00-InitialConfiguration.sh
+++ 00-InitialConfiguration.sh
@@ -2,7 +2,7 @@
 
 # Get Username and write to install scripts
 read -p "Please enter a username: " uname
-sed -i~ -e "s/USERNAME/${uname}/g" initialsetup.sh
+sed -i~ -e "s/USERNAME/${uname}/g" 01-Serverbase.sh
 sed -i~ -e "s/USERNAME/${uname}/g" appinstall.sh
 
 
@@ -17,5 +17,8 @@ sed -i~ -e "s/USEREMAIL/${uemail}/g" appinstall.sh
 # Get Basic Auth Username and write to install scripts
 read -p "Please enter a username to access the websites, (like sonarr and radarr): " unginx
 sed -i~ -e "s/USERBASICAUTH/${unginx}/g" appinstall.sh
+
+chmod +x /home/plex/psrvision/01-Serverbase.sh
+chmod +x /home/plex/psrvision/02-Appsetup.sh
 
 exit
