6a1a027a12edccea0186bd8b6deff0c39dc58494
volvicTM <george.horlor@gmail.com>
2017-09-01 08:12:36 +0100

    Update and rename appinstall.sh to 02-Appsetup.sh

--- 02-Appsetup.sh
+++ 02-Appsetup.sh
@@ -15,12 +15,10 @@ mkdir /home/USERNAME/Radarr
 mkdir /home/USERNAME/Radarr/local
 mkdir /home/USERNAME/Radarr/gdrive
 mkdir /home/USERNAME/Radarr/Media
-mkdir /home/USERNAME/Sabnzbd
-mkdir /home/USERNAME/Sabnzbd/Downloads
-mkdir /home/USERNAME/Sabnzbd
+mkdir /home/USERNAME/Nzbget
 mkdir /home/USERNAME/.config
 mkdir /home/USERNAME/.config/rclone
-mkdir /home/USERNAME/sslcerts
+mkdir /home/USERNAME/Proxy
 echo "- Complete"
 
 echo "Creating Scripts"
@@ -34,10 +32,9 @@ echo "Creating Scripts"
 
 #Mount
 /usr/bin/rclone mount \
---tpslimit 6 \
+--tpslimit 4 \
 --read-only \
 --allow-other \
---stats 1s \
 --quiet \
 --buffer-size 512M \
 --log-file=/home/USERNAME/Scripts/logs/plexmount.log \
@@ -59,7 +56,6 @@ EOM
 /usr/bin/rclone mount \
 --tpslimit 2 \
 --allow-other \
---stats 1s \
 --quiet \
 --buffer-size 512M \
 --log-file=/home/USERNAME/Scripts/logs/sonarrmount.log \
@@ -84,7 +80,6 @@ EOM
 /usr/bin/rclone mount \
 --tpslimit 2 \
 --allow-other \
---stats 1s \
 --quiet \
 --buffer-size 512M \
 --log-file=/home/USERNAME/Scripts/logs/radarrmount.log \
@@ -99,28 +94,27 @@ EOM
 # Make Scripts executable
 chmod +x /home/USERNAME/Scripts/*.sh
 echo "- Complete"
-echo "Installing Apps"
+
 # Install necessary Applications
-# Unzip
-sudo apt-get -y install unzip > /dev/null
-# Fuse
-sudo apt-get -y install fuse > /dev/null
-# Unionfs
-sudo apt-get -y install unionfs-fuse > /dev/null
+echo "Installing Apps"
+sudo apt-get -y install unzip fuse unionfs-fuse> /dev/null
 sudo sed -i 's/#user_allow_other/user_allow_other/g' /etc/fuse.conf
+
 # Docker
 sudo apt-get -y install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common > /dev/null
+apt-transport-https \
+ca-certificates \
+curl \
+software-properties-common > /dev/null
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) \
+stable"
 sudo apt-get -y update > /dev/null
 sudo apt-get -y install docker-ce > /dev/null
+sudo systemctl enable docker
+
 # Rclone
 wget https://downloads.rclone.org/rclone-current-linux-amd64.zip -P /home/USERNAME/Downloads
 unzip /home/USERNAME/Downloads/rclone*.zip -d /home/USERNAME/Downloads/
@@ -128,12 +122,98 @@ sudo cp /home/USERNAME/Downloads/rclone*/rclone /usr/bin
 sudo chown root:root /usr/bin/rclone
 sudo chmod 755 /usr/bin/rclone
 rm -rf /home/USERNAME/Downloads/rclone*
-#touch /home/USERNAME/.config/rclone/rclone.conf
 echo "- Complete"
 
-echo "Setting up Docker Containers"
+# Create isolated docker Network
+docker network create --driver bridge isolated
+
 # Add and run Dockers
-sudo systemctl enable docker
+echo "Setting up Docker Containers"
+
+# Letsencrypt Container (nginx, letsencrypt, fail2ban)
+docker create \
+--privileged \
+--name=letsencrypt \
+--network=isolated \
+-v /home/USERNAME/proxy:/config \
+-e PGID=1000 -e PUID=1000  \
+-e EMAIL=USEREMAIL \
+-e URL=USERURL \
+-e SUBDOMAINS=plex \
+-p 443:443 \
+-e TZ=Europe/London \
+linuxserver/letsencrypt
+sleep 2
+
+# Sonarr Container
+docker create \
+--name sonarr \
+--network=isolated \
+-e PUID=1000 -e PGID=1000 \
+-e TZ=Europe/London \
+-v /etc/localtime:/etc/localtime:ro \
+-v /home/USERNAME/Sonarr:/config \
+-v /home/USERNAME/Sonarr/Media:/tv \
+-v /home/USERNAME/Nzbget/completed:/downloads \
+-v /usr/bin/rclone:/rclone \
+-v /home/USERNAME/.config/rclone:/rcloneconf \
+-v /home/USERNAME/Scripts:/Scripts
+linuxserver/sonarr
+sleep 2
+
+# Radarr Container
+docker create \
+--name=radarr \
+--network=isolated \
+-v /home/USERNAME/Radarr:/config \
+-v /home/USERNAME/Nzbget/completed:/downloads \
+-v /home/USERNAME/Radarr/Media:/movies \
+-v /usr/bin/rclone:/rclone \
+-v /home/USERNAME/.config/rclone:/rcloneconf \
+-v /etc/localtime:/etc/localtime:ro \
+-v /home/USERNAME/Scripts:/Scripts
+-e TZ=Europe/London \
+-e PGID=1000 -e PUID=1000  \
+linuxserver/radarr
+sleep 2
+
+# NZBGet Container
+docker create \
+--name nzbget \
+--network=isolated \
+-e PUID=1000 -e PGID=1000 \
+-e TZ=Europe/London \
+-v /home/USERNAME/Nzbget:/config \
+-v /home/USERNAME/Nzbget/completed:/downloads \
+-v /home/USERNAME/Scripts:/Scripts
+linuxserver/nzbget
+sleep 2
+
+# NZBHydra Container
+docker create \
+--name=hydra \
+--network=isolated \
+-v /home/USERNAME/NzbHydra:/config \
+-v /home/USERNAME/Nzbget:/completed:/downloads \
+-v /home/USERNAME/Scripts:/Scripts
+-e PGID=1000 -e PUID=1000 \
+-e TZ=Europe/London \
+linuxserver/hydra
+sleep 2
+
+# Plex Container
+docker create \
+--name plex \
+--network=isolated \
+-e PLEX_UID=1000 -e PLEX_GID=1000 \
+-e TZ=Europe/London \
+-e PLEX_CLAIM="USERPCLAIM" \
+-v /home/USERNAME/Plex:/config \
+-v /home/USERNAME/Plex:/transcode \
+-v /home/USERNAME/Plex:/data \
+-v /home/USERNAME/Scripts:/Scripts
+plexinc/pms-docker
+sleep 2
 
 echo "- Complete"
 echo "Installation Complete. Please reboot"
