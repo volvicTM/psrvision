2646446309f897ed25c6a6ad1de7c256368181ba
volvicTM <george.horlor@gmail.com>
2017-09-16 08:41:03 +0100

    Update 00-InitialConfiguration.sh

--- 00-InitialConfiguration.sh
+++ 00-InitialConfiguration.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # Get Username and write to install scripts
-read -p "Please enter a username: " uname
+read -p "Enter a username for ubuntu: " uname
 sed -i~ -e "s/USERNAME/${uname}/g" 01-Serverbase.sh
 sed -i~ -e "s/USERNAME/${uname}/g" 02-Appsetup.sh
 sed -i~ -e "s/USERNAME/${uname}/g" Scripts/StartServices.sh
 
 # Get Domain and write to install scripts
-read -p "Please enter a your Domain name (e.g. mydomain.tv): " uurl
+read -p "Enter your Domain name (e.g. mydomain.tv): " uurl
 sed -i~ -e "s/USERURL/${uurl}/g" 02-Appsetup.sh
 sed -i~ -e "s/USERURL/${uurl}/g" Scripts/default
 
 # Get Email and write to install scripts
-read -p "Please enter a email address for let's encrypt: " uemail
+read -p "Enter an email address for let's encrypt: " uemail
 sed -i~ -e "s/USEREMAIL/${uemail}/g" 02-Appsetup.sh
 
 # Get Basic Auth Username and write to install scripts
-read -p "Please enter a username to access the websites, (like sonarr and radarr): " unginx
+read -p "Enter a username to access restricted websites, (like sonarr and radarr): " unginx
 sed -i~ -e "s/USERBASICAUTH/${unginx}/g" 02-Appsetup.sh
 
 chmod +x /root/psrvision/01-Serverbase.sh
