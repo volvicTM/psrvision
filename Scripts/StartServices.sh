b8cb374658e0ddbff77114e4ebcf1791a13f1537
volvicTM <george.horlor@gmail.com>
2017-09-16 09:19:57 +0100

    Update StartServices.sh

--- Scripts/StartServices.sh
+++ Scripts/StartServices.sh
@@ -1,27 +1,41 @@
 #! /bin/bash
 
-# Run Mounts
-sh /home/USERNAME/Scripts/sonarrmount.sh
-sleep 2
-sh /home/USERNAME/Scripts/radarrmount.sh
-sleep 2
-sh /home/USERNAME/Scripts/plexmount.sh
-sleep 2
-
-# Stop all running Containers
+# Unmount mounts and Stop Dockers
+echo "Stopping Containers and unmounting."
 docker stop $(docker ps -a -q) > /dev/null 2>&1
+/bin/fusermount -uz /home/USERNAME/Radarr/gdrive > /dev/null 2>&1
+/bin/fusermount -uz /home/USERNAME/Radarr/local > /dev/null 2>&1
+/bin/fusermount -uz /home/USERNAME/Radarr/Media > /dev/null 2>&1
+/bin/fusermount -uz /home/USERNAME/Sonarr/gdrive > /dev/null 2>&1
+/bin/fusermount -uz /home/USERNAME/Sonarr/local > /dev/null 2>&1
+/bin/fusermount -uz /home/USERNAME/Sonarr/Media > /dev/null 2>&1
+/bin/fusermount -uz /home/USERNAME/Plex/Media > /dev/null 2>&1
+echo "- Completed"
 
-# Start Containers in Proxy Order
-docker start letsencrypt
+# Start Containers
+echo "Starting Containers"
+docker start letsencrypt > /dev/null 2>&1
 sleep 3
-docker start nzbget
+docker start plex > /dev/null 2>&1
 sleep 3
-docker start sonarr
+docker start sonarr > /dev/null 2>&1
 sleep 3
-docker start radarr
+docker start radarr > /dev/null 2>&1
 sleep 3
-docker start hydra
+docker start hydra > /dev/null 2>&1
 sleep 3
-docker start plex
+docker start nzbget > /dev/null 2>&1
+sleep 3
+echo "- Completed"
+
+# Run Mounts
+echo "Mounting Google Drive Mounts"
+sh /home/USERNAME/Scripts/sonarrmount.sh > /dev/null 2>&1
+sleep 2
+sh /home/USERNAME/Scripts/radarrmount.sh > /dev/null 2>&1
+sleep 2
+sh /home/USERNAME/Scripts/plexmount.sh > /dev/null 2>&1
+sleep 2
+echo "- Completed"
 
 exit
