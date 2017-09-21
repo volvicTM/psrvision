d4f3f6c07284c72f1b7c6300c9b2005cd59f95a5
volvicTM <george.horlor@gmail.com>
2017-09-21 17:39:33 +0100

    Update 02-Appsetup.sh

--- 02-Appsetup.sh
+++ 02-Appsetup.sh
@@ -32,7 +32,7 @@ echo "- Complete"
 
 # Install necessary Applications
 echo "Installing Apps"
-sudo apt-get -y install unzip fuse unionfs-fuse> /dev/null 2>&1
+sudo apt-get -y install unzip fuse unionfs-fuse
 sudo sed -i 's/#user_allow_other/user_allow_other/g' /etc/fuse.conf
 
 # Docker
@@ -40,24 +40,24 @@ sudo apt-get -y install \
 apt-transport-https \
 ca-certificates \
 curl \
-software-properties-common > /dev/null 2>&1
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - > /dev/null 2>&1
+software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
 "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
 $(lsb_release -cs) \
-stable" > /dev/null 2>&1
-sudo apt-get -y update > /dev/null 2>&1
-sudo apt-get -y install docker-ce > /dev/null 2>&1
-sudo systemctl enable docker > /dev/null 2>&1
+stable"
+sudo apt-get -y update
+sudo apt-get -y install docker-ce
+sudo systemctl enable docker
 sudo usermod -aG docker USERNAME
 
 # Rclone
-wget https://downloads.rclone.org/rclone-current-linux-amd64.zip -P /home/USERNAME/Downloads > /dev/null 2>&1
-unzip /home/USERNAME/Downloads/rclone*.zip -d /home/USERNAME/Downloads/ > /dev/null 2>&1
-sudo cp /home/USERNAME/Downloads/rclone*/rclone /usr/local/sbin/rclone > /dev/null 2>&1
-sudo chown root:root /usr/local/sbin/rclone > /dev/null 2>&1
-sudo chmod 755 /usr/local/sbin/rclone > /dev/null 2>&1
-rm -rf /home/USERNAME/Downloads/rclone* > /dev/null 2>&1
+wget https://downloads.rclone.org/rclone-current-linux-amd64.zip -P /home/USERNAME/Downloads
+unzip /home/USERNAME/Downloads/rclone*.zip -d /home/USERNAME/Downloads/
+sudo cp /home/USERNAME/Downloads/rclone*/rclone /usr/local/sbin/rclone
+sudo chown root:root /usr/local/sbin/rclone
+sudo chmod 755 /usr/local/sbin/rclone
+rm -rf /home/USERNAME/Downloads/rclone*
 echo "- Complete"
 
 # Create isolated docker Network
@@ -82,8 +82,8 @@ docker create \
 -e SUBDOMAINS=plex \
 -p 443:443 \
 -e TZ=Europe/London \
-linuxserver/letsencrypt > /dev/null 2>&1
-sleep 15
+linuxserver/letsencrypt
+sleep 20
 
 # Sonarr Container
 docker create \
@@ -99,8 +99,8 @@ docker create \
 -v /usr/local/sbin:/rclone \
 -v /home/USERNAME/.config/rclone:/rcloneconf \
 -v /home/USERNAME/Scripts:/Scripts \
-linuxserver/sonarr > /dev/null 2>&1
-sleep 15
+linuxserver/sonarr
+sleep 20
 
 # Radarr Container
 docker create \
@@ -116,8 +116,8 @@ docker create \
 -v /etc/localtime:/etc/localtime:ro \
 -e TZ=Europe/London \
 -e PGID=USERGID -e PUID=USERUID \
-linuxserver/radarr > /dev/null 2>&1
-sleep 15
+linuxserver/radarr
+sleep 20
 
 # NZBGet Container
 docker create \
@@ -129,8 +129,8 @@ docker create \
 -v /home/USERNAME/Nzbget:/config \
 -v /home/USERNAME/Nzbget:/downloads \
 -v /home/USERNAME/Scripts:/Scripts \
-linuxserver/nzbget > /dev/null 2>&1
-sleep 15
+linuxserver/nzbget
+sleep 20
 
 # NZBHydra Container
 docker create \
@@ -142,8 +142,8 @@ docker create \
 -v /home/USERNAME/Scripts:/Scripts \
 -e PGID=USERGID -e PUID=USERUID \
 -e TZ=Europe/London \
-linuxserver/hydra > /dev/null 2>&1
-sleep 15
+linuxserver/hydra
+sleep 20
 
 # Plex Container
 docker create \
@@ -157,24 +157,24 @@ docker create \
 -v /home/USERNAME/Plex:/transcode \
 -v /home/USERNAME/Plex:/data \
 -v /home/USERNAME/Scripts:/Scripts \
-plexinc/pms-docker > /dev/null 2>&1
-sleep 10
+plexinc/pms-docker
+sleep 20
 echo "- Complete"
 
 # Configure Dockers and Proxy
 echo "Configuring Containers"
 # Start dockers to build configuration files
-docker start letsencrypt > /dev/null 2>&1
+docker start letsencrypt
 sleep 10
-docker start nzbget > /dev/null 2>&1
+docker start nzbget
 sleep 5
-docker start sonarr > /dev/null 2>&1
+docker start sonarr
 sleep 5
-docker start radarr > /dev/null 2>&1
+docker start radarr
 sleep 5
-docker start hydra > /dev/null 2>&1
+docker start hydra
 sleep 5
-docker start plex > /dev/null 2>&1
+docker start plex
 sleep 5
 
 # Configure Lets Encrypt
@@ -182,30 +182,30 @@ echo "Please enter password to access restricted sites (sonarr, radarr, nzbget a
 docker exec -it letsencrypt htpasswd -c /config/nginx/.htpasswd USERBASICAUTH
 sudo rm /home/USERNAME/Proxy/nginx/site-confs/default
 sudo cp /home/USERNAME/psrvision/Scripts/default /home/USERNAME/Proxy/nginx/site-confs/default
-docker restart letsencrypt > /dev/null 2>&1
+docker restart letsencrypt
 sleep 5
 
 # Configure Sonarr
 sed -i~ -e 's=<UrlBase></UrlBase>=<UrlBase>/tv</UrlBase>=g' /home/USERNAME/Sonarr/config.xml
-docker restart sonarr > /dev/null 2>&1
+docker restart sonarr
 sleep 5
 
 # Configure Radarr
 sed -i~ -e 's=<UrlBase></UrlBase>=<UrlBase>/film</UrlBase>=g' /home/USERNAME/Radarr/config.xml
-docker restart radarr > /dev/null 2>&1
+docker restart radarr
 sleep 5
 
 # Configure Hydra
 sed -i~ -e 's="urlBase": null,="urlBase": "/hydra",=g' /home/USERNAME/NzbHydra/hydra/settings.cfg
-docker restart hydra > /dev/null 2>&1
+docker restart hydra
 sleep 5
 
 # Configure Nzbget
 sed -i 's/ControlUsername=nzbget/ControlUsername=/' /home/USERNAME/Nzbget/nzbget.conf
 sed -i 's/ControlPassword=tegbzn6789/ControlPassword=/' /home/USERNAME/Nzbget/nzbget.conf
-docker restart nzbget > /dev/null 2>&1
+docker restart nzbget
 
-docker stop $(docker ps -a -q) > /dev/null 2>&1
+docker stop $(docker ps -a -q)
 echo "- Complete"
 echo "Please record your username and password. (You may change the password at any time!)"
 echo
