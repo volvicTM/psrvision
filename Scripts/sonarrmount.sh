96c20c9c6c42e26c396e1b9a6d37453d586f00e6
volvicTM <george.horlor@gmail.com>
2017-09-22 16:32:06 +0100

    Update sonarrmount.sh

--- Scripts/sonarrmount.sh
+++ Scripts/sonarrmount.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 #Unmount
-/bin/fusermount -uz /home/USERNAME/Sonarr/Media
-/bin/fusermount -uz /home/USERNAME/Sonarr/gdrive
-/bin/fusermount -uz /home/USERNAME/Sonarr/local
+/bin/fusermount -uz /home/USERNAME/Mount/Sonarr/Media
+/bin/fusermount -uz /home/USERNAME/Mount/Sonarr/gdrive
+/bin/fusermount -uz /home/USERNAME/Mount/Sonarr/local
 
 #Mount
 /usr/local/sbin/rclone mount \
@@ -12,9 +12,9 @@
 --quiet \
 --buffer-size 512M \
 --log-file=/home/USERNAME/Scripts/logs/sonarrmount.log \
-Sonarr_Crypt: /home/USERNAME/Sonarr/gdrive &
+Sonarr_Crypt: /home/USERNAME/Mount/Sonarr/gdrive &
 
 #UnionFuse Local and gdrive into Media
-unionfs-fuse -o cow,allow_other,direct_io,auto_cache,sync_read /home/USERNAME/Sonarr/local=RW:/home/USERNAME/Sonarr/gdrive=RO /home/USERNAME/Sonarr/Media/
+unionfs-fuse -o cow,allow_other,direct_io,auto_cache,sync_read /home/USERNAME/Mount/Sonarr/local=RW:/home/USERNAME/Mount/Sonarr/gdrive=RO /home/USERNAME/Mount/Sonarr/Media/
 
 exit
