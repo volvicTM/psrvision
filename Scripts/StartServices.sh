c52a49bdd491cbcab942992db9bf839ce40da898
volvicTM <george.horlor@gmail.com>
2017-09-22 16:27:02 +0100

    Update StartServices.sh

--- Scripts/StartServices.sh
+++ Scripts/StartServices.sh
@@ -3,13 +3,13 @@
 # Unmount mounts and Stop Dockers
 echo "Stopping Containers and unmounting."
 docker stop $(docker ps -a -q) > /dev/null 2>&1
-/bin/fusermount -uz /home/USERNAME/Radarr/gdrive > /dev/null 2>&1
-/bin/fusermount -uz /home/USERNAME/Radarr/local > /dev/null 2>&1
-/bin/fusermount -uz /home/USERNAME/Radarr/Media > /dev/null 2>&1
-/bin/fusermount -uz /home/USERNAME/Sonarr/gdrive > /dev/null 2>&1
-/bin/fusermount -uz /home/USERNAME/Sonarr/local > /dev/null 2>&1
-/bin/fusermount -uz /home/USERNAME/Sonarr/Media > /dev/null 2>&1
-/bin/fusermount -uz /home/USERNAME/Plex/Media > /dev/null 2>&1
+/bin/fusermount -uz /home/USERNAME/Mount/Radarr/gdrive > /dev/null 2>&1
+/bin/fusermount -uz /home/USERNAME/Mount/Radarr/local > /dev/null 2>&1
+/bin/fusermount -uz /home/USERNAME/Mount/Radarr/Media > /dev/null 2>&1
+/bin/fusermount -uz /home/USERNAME/Mount/Sonarr/gdrive > /dev/null 2>&1
+/bin/fusermount -uz /home/USERNAME/Mount/Sonarr/local > /dev/null 2>&1
+/bin/fusermount -uz /home/USERNAME/Mount/Sonarr/Media > /dev/null 2>&1
+/bin/fusermount -uz /home/USERNAME/Mount/Plex/Media > /dev/null 2>&1
 echo "- Completed"
 
 # Start Containers
