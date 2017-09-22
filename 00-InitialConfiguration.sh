6540ac2dadb15957e94162d6239a6eec3fc31902
volvicTM <george.horlor@gmail.com>
2017-09-22 08:27:34 +0100

    Update 00-InitialConfiguration.sh

--- 00-InitialConfiguration.sh
+++ 00-InitialConfiguration.sh
@@ -4,20 +4,21 @@
 read -p "Enter a username for ubuntu: " uname
 sed -i~ -e "s/USERNAME/${uname}/g" 01-Serverbase.sh
 sed -i~ -e "s/USERNAME/${uname}/g" 02-Appsetup.sh
+sed -i~ -e "s/USERNAME/${uname}/g" 03-DockerSetup.sh
 sed -i~ -e "s/USERNAME/${uname}/g" Scripts/StartServices.sh
 
 # Get Domain and write to install scripts
 read -p "Enter your Domain name (e.g. mydomain.tv): " uurl
-sed -i~ -e "s/USERURL/${uurl}/g" 02-Appsetup.sh
+sed -i~ -e "s/USERURL/${uurl}/g" 03-DockerSetup.sh
 sed -i~ -e "s/USERURL/${uurl}/g" Scripts/default
 
 # Get Email and write to install scripts
 read -p "Enter an email address for let's encrypt: " uemail
-sed -i~ -e "s/USEREMAIL/${uemail}/g" 02-Appsetup.sh
+sed -i~ -e "s/USEREMAIL/${uemail}/g" 03-DockerSetup.sh
 
 # Get Basic Auth Username and write to install scripts
 read -p "Enter a username to access restricted websites, (like sonarr and radarr): " unginx
-sed -i~ -e "s/USERBASICAUTH/${unginx}/g" 02-Appsetup.sh
+sed -i~ -e "s/USERBASICAUTH/${unginx}/g" 03-DockerSetup.sh
 
 chmod +x /root/psrvision/01-Serverbase.sh
 chmod +x /root/psrvision/02-Appsetup.sh
