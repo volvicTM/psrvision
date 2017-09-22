18dbc1c8fe0299007890c9aa10a418a321ab8bfa
volvicTM <george.horlor@gmail.com>
2017-09-22 16:07:20 +0100

    Update 00-InitialConfiguration.sh

--- 00-InitialConfiguration.sh
+++ 00-InitialConfiguration.sh
@@ -2,27 +2,24 @@
 
 # Get Username and write to install scripts
 read -p "Enter a username for ubuntu: " uname
-sed -i~ -e "s/USERNAME/${uname}/g" 01-Serverbase.sh
-sed -i~ -e "s/USERNAME/${uname}/g" 02-Appsetup.sh
-sed -i~ -e "s/USERNAME/${uname}/g" 03-DockerSetup.sh
-sed -i~ -e "s/USERNAME/${uname}/g" Scripts/StartServices.sh
+sed -i~ -e "s/USERNAME/${uname}/g" *.sh
+sed -i~ -e "s/USERNAME/${uname}/g" Scripts/*.sh
 
 # Get Domain and write to install scripts
 read -p "Enter your Domain name (e.g. mydomain.tv): " uurl
-sed -i~ -e "s/USERURL/${uurl}/g" 03-DockerSetup.sh
-sed -i~ -e "s/USERURL/${uurl}/g" Scripts/default
+sed -i~ -e "s/USERURL/${uurl}/g" *.sh
+sed -i~ -e "s/USERURL/${uurl}/g" Scripts/*
 
 # Get Email and write to install scripts
 read -p "Enter an email address for let's encrypt: " uemail
-sed -i~ -e "s/USEREMAIL/${uemail}/g" 03-DockerSetup.sh
+sed -i~ -e "s/USEREMAIL/${uemail}/g" *.sh
 
 # Get Basic Auth Username and write to install scripts
 read -p "Enter a username to access restricted websites, (like sonarr and radarr): " unginx
-sed -i~ -e "s/USERBASICAUTH/${unginx}/g" 03-DockerSetup.sh
+sed -i~ -e "s/USERBASICAUTH/${unginx}/g" *.sh
 
-chmod +x /root/psrvision/01-Serverbase.sh
-chmod +x /root/psrvision/02-Appsetup.sh
-chmod +x /root/psrvision/Scripts/StartServices.sh
+chmod +x /root/psrvision/*.sh
+chmod +x /root/psrvision/Scripts/*.sh
 sh 01-Serverbase.sh
 
 exit
